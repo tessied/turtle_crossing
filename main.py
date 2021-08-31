3405713a1ea823fe89b026207b1ef0f871f91259
tessied <tessied@uchicago.edu>
2021-08-31 17:16:35 -0400

    Implement turtle movement

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+turtle = Player()
+
+screen.listen()
+screen.onkey(turtle.move, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
