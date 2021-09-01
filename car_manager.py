258fd973db0b8d7e797a345faeaa3d92dffbaa28
tessied <tessied@uchicago.edu>
2021-08-31 20:08:02 -0400

    Implement car movement

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,27 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+
+    def __init__(self):
+        self.all_cars = []
+
+    def new_car(self):
+        car = Turtle(shape="square")
+        car.pu()
+        car.shapesize(stretch_wid=1, stretch_len=2)
+        car.setheading(180)
+        car.goto(300, random.randint(-240, 240))
+        car.color(random.choice(COLORS))
+        self.all_cars.append(car)
+
+    def move_car(self):
+        for car in self.all_cars:
+            car.forward(STARTING_MOVE_DISTANCE)
+
+
--- main.py
+++ main.py
@@ -5,15 +5,24 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
+screen.title("Turtle Crossing")
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
 turtle = Player()
+cars = CarManager()
 
 screen.listen()
 screen.onkey(turtle.move, "Up")
 
 game_is_on = True
+count = 0
+
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    cars.move_car()
+    if count % 6 == 0:
+        cars.new_car()
+    count += 1
